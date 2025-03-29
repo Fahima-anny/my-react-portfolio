57945a31ec3e29a0446cf360c4a8d4b7e4650651
Fahima-anny <fahima.info03@gmail.com>
2025-03-29 07:45:20 +0600

    project data updated

--- src/Compoonents/Layouts/Home/Projects/ProjectCard.jsx
+++ src/Compoonents/Layouts/Home/Projects/ProjectCard.jsx
@@ -4,20 +4,22 @@ import { Link } from "react-router-dom";
 
 const ProjectCard = ({project}) => {
 
-    const {id, projectImage, projectName, liveLink} = project ;
+    const {id, projectImage, projectName, liveLink, details} = project ;
 
     return (
         <div>
-              <div className="hero h-[400px] duration-500 hover:shadow-md hover:shadow-gray-600 rounded-md "
-                  style={{
-                    backgroundImage: `url(${projectImage})`,
-                  }}
+              <div className=" h-[400px] duration-500 bg-contain"
+                  // style={{
+                  //   backgroundImage: `url(${projectImage})`,
+                  // }}
                 >
-                <div className="hero-overlay bg-black bg-opacity-70 rounded-md"></div>
-                <div className="hero-content text-center text-neutral-content">
-                  <div className="flex flex-col h-[350px] justify-start gap-[250px] ">
-                    <h1 className="mb-5 text-2xl text-white font-bold">{projectName}</h1>
-                  <div className="flex gap-3">
+                  <img src={projectImage} alt="" />
+                {/* <div className="hero-overlay bg-black bg-opacity-70 rounded-md"></div> */}
+
+                  <div className="flex flex-col justify-start ">
+                    <h1 className="mt-3 mb-1 text-2xl text-center text-white font-bold">{projectName}</h1>
+                    <p className="text-gray-400 pb-3 ">{details.slice(0, 120)}{details.length > 120 ? "..." : ""}</p>
+                  {/* <div className="flex gap-3 justify-between ">
                   <Link
                     to={`/ProjectDetails/${id}`}
                         className="btn bg-orange-400 text-black hover:bg-orange-400 hover:ring-1 hover:ring-white duration-300">View
@@ -26,8 +28,8 @@ const ProjectCard = ({project}) => {
                     to={liveLink}
                     target="_blank"
                         className="btn md:w-[140px] bg-orange-400 text-black hover:bg-orange-400 hover:ring-1 hover:ring-white duration-300">Live Link</Link>
-                  </div>
-                  </div>
+
+                  </div> */}
                 </div>
               </div>
         </div>
