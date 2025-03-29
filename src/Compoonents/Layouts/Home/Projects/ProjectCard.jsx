77c14cefc8360e022ff6e7ca995112a24dedc82a
Fahima-anny <fahima.info03@gmail.com>
2025-03-29 07:57:24 +0600

    project card updated

--- src/Compoonents/Layouts/Home/Projects/ProjectCard.jsx
+++ src/Compoonents/Layouts/Home/Projects/ProjectCard.jsx
@@ -8,14 +8,29 @@ const ProjectCard = ({project}) => {
 
     return (
         <div>
-              <div className=" h-[400px] duration-500 bg-contain"
-                  // style={{
-                  //   backgroundImage: `url(${projectImage})`,
-                  // }}
-                >
-                  <img src={projectImage} alt="" />
-                {/* <div className="hero-overlay bg-black bg-opacity-70 rounded-md"></div> */}
-
+              <div className="relative group">
+      {/* Image Container */}
+      <div className="duration-500 bg-contain relative overflow-hidden">
+        {/* Image */}
+        <img src={projectImage} className="w-full h-full object-cover" alt="" />
+        
+        {/* Black Overlay (Initially Hidden, Appears on Hover) */}
+        <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-60 transition-opacity duration-300">
+        <div className="flex h-full justify-center items-center gap-10 opacity-100">
+              <Link
+                to={`/ProjectDetails/${id}`}
+                className="btn bg-orange-400 text-black hover:bg-orange-400 hover:ring-1 hover:ring-white duration-300">
+                View Details
+              </Link>
+              <Link
+                to={liveLink}
+                target="_blank"
+                className="btn md:w-[140px] bg-orange-400 text-black hover:bg-orange-400 hover:ring-1 hover:ring-white duration-300">
+                Live Link
+              </Link>
+            </div>
+        </div>
+      </div>
                   <div className="flex flex-col justify-start ">
                     <h1 className="mt-3 mb-1 text-2xl text-center text-white font-bold">{projectName}</h1>
                     <p className="text-gray-400 pb-3 ">{details.slice(0, 120)}{details.length > 120 ? "..." : ""}</p>
